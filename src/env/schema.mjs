6895470d1b1a728d509c998d073e54bf04b1581a
ahmedpasic19 <ahmedpasic15@gmail.com>
2023-09-17 16:59:34 +0200

    Removed unnecessary .env variables

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -8,21 +8,16 @@ import { z } from 'zod'
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(['development', 'test', 'production']),
-  NEXTAUTH_SECRET:
-    process.env.NODE_ENV === 'production'
-      ? z.string().min(1)
-      : z.string().min(1).optional(),
+  NEXTAUTH_SECRET: process.env.NODE_ENV === 'production' ? z.string().min(1) : z.string().min(1).optional(),
   NEXTAUTH_URL: z.preprocess(
     // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
     // Since NextAuth.js automatically uses the VERCEL_URL if present.
     (str) => process.env.VERCEL_URL ?? str,
     // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-    process.env.VERCEL ? z.string() : z.string().url()
+    process.env.VERCEL ? z.string() : z.string().url(),
   ),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
-  SECRET_ACCES_KEY: z.string(),
-  ACCESS_KEY: z.string(),
 })
 
 /**
@@ -37,8 +32,6 @@ export const serverEnv = {
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  SECRET_ACCES_KEY: process.env.SECRET_ACCES_KEY,
-  ACCESS_KEY: process.env.ACCESS_KEY,
 }
 
 /**
